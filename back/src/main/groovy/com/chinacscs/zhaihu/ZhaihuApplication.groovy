42868467c3aafaef5cb4d88a2a14060e0ff99106
xugq <xugq@chinacscs.com>
2018-04-10 18:57:51 +0800

    initial commit

---
+++ back/src/main/groovy/com/chinacscs/zhaihu/ZhaihuApplication.groovy
@@ -0,0 +1,12 @@
+package com.chinacscs.zhaihu
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ZhaihuApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ZhaihuApplication, args
+	}
+}
